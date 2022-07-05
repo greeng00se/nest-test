6ec8f049b4cfbcee14e569d72aec77d9bedc651d
greeng00se <thegreengoose7@gmail.com>
2022-07-05 22:26:25 +0900

    Swagger 접근 제한하기

--- swagger/src/main.ts
+++ swagger/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './config/SwaggerConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Swagger 접근 제한
+  app.use(
+    ['/api'],
+    expressBasicAuth({
+      challenge: true,
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PWD },
+    }),
+  );
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
